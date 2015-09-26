ac0d75514f00524c2864d4ebc1ec7080ce05809d
Imdad Ahmed <imdhmd@gmail.com>
2015-09-26 14:19:43 +0530

    melpa packages for emacs

--- .emacs.d/requires.el
+++ .emacs.d/requires.el
@@ -1,4 +1,5 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("meta-stable" . "http://melpa-stable.milkbox.net/packages/"))
+	     '("meta-stable" . "http://melpa-stable.milkbox.net/packages/")
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
