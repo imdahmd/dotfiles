3e40c04a7f118970147683a14115858b9a181a04
Imdad Ahmed <imdhmd@gmail.com>
2015-10-13 17:06:03 +0530

    Sets github token for paradox package manager

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,3 +14,17 @@
 (load-user-file "requires.el")
 (load-user-file "clojure.el")
 (load-user-file "java.el")
+(load-user-file "paradox-github-token.el")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(paradox-automatically-star t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
