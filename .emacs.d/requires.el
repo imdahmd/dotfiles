76ca23618b073d14f10a955fca7b39d893a04467
Imdad Ahmed <imdhmd@gmail.com>
2015-10-13 17:06:38 +0530

    Removes now-defunct url for melpa stable

--- .emacs.d/requires.el
+++ .emacs.d/requires.el
@@ -1,5 +1,4 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("meta-stable" . "http://melpa-stable.milkbox.net/packages/")
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
