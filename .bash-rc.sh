c124abf485f74091be11fba2b17f8df26d812002
Imdad Ahmed <imdhmd@gmail.com>
2015-12-01 23:21:39 -0500

    Adds setting to define ls colors for black shell

--- .bash-rc.sh
+++ .bash-rc.sh
@@ -1,2 +1,5 @@
 [[ -s "$HOME/.aliases-rc.sh" ]] && source "$HOME/.aliases-rc.sh"
 [[ -s "$HOME/.history-rc.sh" ]] && source "$HOME/.history-rc.sh"
+
+## Stuff that doesn't fit in any of above files, and doesn't (yet) deserve a file of its own:
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx # LSCOLORS for black background shell
