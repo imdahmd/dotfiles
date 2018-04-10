964fb1a413ee2d6e341400453d7a97e1dfe37598
Imdad Ahmed <imdhmd@gmail.com>
2018-04-10 17:38:42 +0100

    Add aliases to docker, generating uuid and to clone a git project

--- .aliases-rc.sh
+++ .aliases-rc.sh
@@ -9,3 +9,12 @@ alias rerc='source ~/.bash_profile'
 alias mci='mvn clean install'
 alias mcis='mci -DskipTests'
 alias v=vagrant
+alias dm=docker-machine
+alias uuidgen='uuidgen | tr "[:upper:]" "[:lower:]" | tr -d "\n"'
+
+function cloned {
+    URL=$1
+    REPO=`basename "$URL" .git`
+
+    g clone $URL && cd $REPO
+}
