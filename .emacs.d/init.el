3f9cc5d12f454c384dd4ffea3d42abb181ad3848
Imdad Ahmed <imdhmd@gmail.com>
2018-04-10 17:38:43 +0100

    Add emacs 25 specific custom configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defconst user-init-dir
   (cond ((boundp 'user-emacs-directory)
 	 user-emacs-directory)
@@ -28,6 +35,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (projectile paradox gradle-mode clj-refactor better-defaults)))
  '(paradox-automatically-star t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
