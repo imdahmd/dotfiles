dd5cc7a652a17cc751cea88c6596d5426004dde2
Imdad Ahmed <imdhmd@gmail.com>
2018-04-10 17:38:43 +0100

    Add docker, ansible, rvm and nvm bash configuration

--- .bash-rc.sh
+++ .bash-rc.sh
@@ -5,3 +5,11 @@
 export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx # LSCOLORS for black background shell
 
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
+# if which docker-machine > /dev/null; then eval "$(docker-machine env default)"; fi
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+export PATH="/usr/local/opt/ansible@2.0/bin:$PATH"
+export PATH="$HOME/.rvm/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+. "/usr/local/opt/nvm/nvm.sh"
