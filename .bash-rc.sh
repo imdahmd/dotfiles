13537f6f29e1515c8063fd96354e8c4bdc6d41cd
Imdad Ahmed <imdhmd@gmail.com>
2018-08-29 10:30:30 +0100

    Refactor bash init scripts

--- .bash-rc.sh
+++ .bash-rc.sh
@@ -1,21 +1,6 @@
-[[ -s "$HOME/.aliases-rc.sh" ]] && source "$HOME/.aliases-rc.sh"
-[[ -s "$HOME/.history-rc.sh" ]] && source "$HOME/.history-rc.sh"
-
-## Stuff that doesn't fit in any of above files, and doesn't (yet) deserve a file of its own:
 export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx # LSCOLORS for black background shell
 
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
-# if which docker-machine > /dev/null; then eval "$(docker-machine env default)"; fi
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-export PATH="/usr/local/opt/ansible@2.0/bin:$PATH"
-export PATH="$HOME/.rvm/bin:$PATH"
 
-export NVM_DIR="$HOME/.nvm"
-. "/usr/local/opt/nvm/nvm.sh"
-
-hitch() {
-  command hitch "$@"
-  if [[ -s "$HOME/.hitch_export_authors" ]] ; then source "$HOME/.hitch_export_authors" ; fi
-}
-alias unhitch='hitch -u'
--- .bash_profile
+++ .bash_profile
@@ -1,2 +1,4 @@
 [[ -s "$HOME/.bash-rc.sh" ]] && source "$HOME/.bash-rc.sh"
+[[ -s "$HOME/.aliases-rc.sh" ]] && source "$HOME/.aliases-rc.sh"
+[[ -s "$HOME/.history-rc.sh" ]] && source "$HOME/.history-rc.sh"
 [[ -s "$HOME/.bash-org.sh" ]] && source "$HOME/.bash-org.sh"
