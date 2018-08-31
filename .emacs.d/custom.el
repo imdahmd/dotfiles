19576f8493d197136a5a34c405b31dcb4e94dec3
Imdad Ahmed <imdhmd@gmail.com>
2018-09-01 00:32:24 +0100

    Install cider

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (markdown-mode projectile better-defaults))))
+ '(package-selected-packages
+   (quote
+    (cider smartparens markdown-mode projectile better-defaults)))
+ '(smartparens-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
