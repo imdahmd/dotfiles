e26f7ceb5dec89a0cadd3e08e392578486ff0b98
Imdad Ahmed <imdhmd@gmail.com>
2018-09-21 08:41:50 +0100

    Add key bindings for projectile

--- .emacs.d/projectile.el
+++ .emacs.d/projectile.el
@@ -1,4 +1,8 @@
 (unless (package-installed-p 'projectile)
   (package-install 'projectile))
 
-(setq projectile-project-search-path '("~/Projects/"))
+(setq projectile-project-search-path '("~"))
+
+;; https://projectile.readthedocs.io/en/latest/installation/
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
