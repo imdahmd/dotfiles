4fae8c1881805cc4106c8d10e74bbe2912004951
Imdad Ahmed <imdad.ahmed@tesco.com>
2019-03-11 09:56:00 +0000

    Add gopath

--- .bash-rc.sh
+++ .bash-rc.sh
@@ -4,4 +4,8 @@ export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx # LSCOLORS for black background shell
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
+if [ -d $HOME/Projects/golang ]; then
+    export GOPATH="$HOME/Projects/golang"
+    export GOBIN="$GOPATH/bin"
+fi
 
