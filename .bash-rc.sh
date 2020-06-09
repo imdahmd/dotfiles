e56c513ffebe0a2e1ab4a13e15fba8b60f9e8d08
Imdad Ahmed <imdad.ahmed@gmail.com>
2020-06-09 12:01:41 +0100

    Configure nvm

--- .bash-rc.sh
+++ .bash-rc.sh
@@ -9,3 +9,9 @@ if [ -d $HOME/Projects/golang ]; then
     export GOBIN="$GOPATH/bin"
 fi
 
+if [ -d $HOME/.nvm ]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    export PATH="/usr/local/sbin:$PATH"
+fi
