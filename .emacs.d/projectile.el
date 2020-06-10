e66d5e7e1bc82eb5f7fa425bb2e30e0c50ff30e6
Imdad Ahmed <imdad.ahmed@gmail.com>
2020-06-10 17:59:27 +0100

    Define keybinding for projectile

--- .emacs.d/projectile.el
+++ .emacs.d/projectile.el
@@ -4,5 +4,7 @@
 (setq projectile-project-search-path '("~"))
 
 ;; https://projectile.readthedocs.io/en/latest/installation/
-(projectile-mode +1)
+(require 'projectile)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
