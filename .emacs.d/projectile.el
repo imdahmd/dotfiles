41e96f831c7b132b2c1dcd0c30925f2231c5dc65
Imdad Ahmed <imdad.ahmed@gmail.com>
2021-03-06 20:02:59 +0000

    Tell projectile (emacs) where to look for projects

--- .emacs.d/projectile.el
+++ .emacs.d/projectile.el
@@ -8,3 +8,7 @@
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (projectile-mode +1)
+
+;; Discover projects
+;; https://docs.projectile.mx/projectile/usage.html#basic-setup
+(setq projectile-project-search-path '("~/Projects/" "~/Personal/" "~/ThoughtWorks/"))
