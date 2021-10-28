5e52572191b7d9e73acbbf0c36798a73b9312cc8
Imdad Ahmed <imdad.ahmed@gmail.com>
2021-10-28 12:18:55 +0100

    Add clj-refactor to emacs and add supported functions to multi cursor

--- .emacs.d/.mc-lists.el
+++ .emacs.d/.mc-lists.el
@@ -3,14 +3,22 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        backward-sexp
         beginning-of-visual-line
         cider-eval-last-sexp
         downcase-region
         electric-newline-and-maybe-indent
         end-of-visual-line
+        forward-sexp
         indent-for-tab-command
+        kill-region
         kill-visual-line
         markdown-insert-list-item
+        paredit-backward
+        paredit-backward-kill-word
+        paredit-forward
+        paredit-forward-delete
+        paredit-forward-kill-word
         sp-backward-unwrap-sexp
         sp-forward-barf-sexp
         sp-forward-slurp-sexp
