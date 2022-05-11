e3e425833da77b286c695d5727523daf547f4a21
Imdad Ahmed <imdad.ahmed@gmail.com>
2022-05-11 17:05:55 +0100

    Setup path to sbin correctly

--- .bash-rc.sh
+++ .bash-rc.sh
@@ -14,5 +14,6 @@ if [ -d $HOME/.nvm ]; then
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    export PATH="/usr/local/sbin:$PATH"
 fi
+
+export PATH="/usr/local/sbin:$PATH"
