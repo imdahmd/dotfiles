f23dc46dcbcef2caf7c8592d3db5e6bbe9afe26e
Imdad Ahmed <imdad.ahmed@gmail.com>
2022-06-05 20:33:46 +0100

    Add elpa mirrors

--- .emacs.d/requires.el
+++ .emacs.d/requires.el
@@ -1,5 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/")
-	     '("marmalade" . "https://marmalade-repo.org/packages/"))
+	     '("marmalade" . "https://marmalade-repo.org/packages/")
+       '("org"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/org/")
+       '("gnu"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/gnu/"))
 (package-initialize)
