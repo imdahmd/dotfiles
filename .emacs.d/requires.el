ffb76f17751d4a1dbcc0958307f5ce3effc5aa9a
Imdad Ahmed <imdad.ahmed@gmail.com>
2022-06-06 18:12:01 +0100

    Revert "Add elpa mirrors"
    This reverts commit f23dc46dcbcef2caf7c8592d3db5e6bbe9afe26e.

--- .emacs.d/requires.el
+++ .emacs.d/requires.el
@@ -1,7 +1,5 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/")
-	     '("marmalade" . "https://marmalade-repo.org/packages/")
-       '("org"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/org/")
-       '("gnu"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/gnu/"))
+	     '("marmalade" . "https://marmalade-repo.org/packages/"))
 (package-initialize)
