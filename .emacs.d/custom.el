bb7565d01533bbcd01e7023aff91c62c85bdb794
Imdad Ahmed <imdad.ahmed@gmail.com>
2022-11-11 19:21:31 +0000

    Add magit to emacs

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ns-command-modifier 'super)
- '(package-selected-packages
-   '(paredit clojure-mode))
+ '(package-selected-packages '(magit yaml-mode paredit clojure-mode))
  '(ring-bell-function 'ignore)
  '(visible-bell t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
