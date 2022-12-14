7f248219db0a3496b9ab2559620b838c3d28e68e
Imdad Ahmed <imdad.ahmed@gmail.com>
2022-12-14 06:00:54 +0000

    Refresh melpa contents, install cider and tomorrow theme

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,8 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(ns-command-modifier 'super)
- '(package-selected-packages '(magit yaml-mode paredit clojure-mode))
+ '(package-selected-packages
+   '(color-theme-sanityinc-tomorrow cider magit yaml-mode paredit clojure-mode))
  '(ring-bell-function 'ignore)
  '(visible-bell t))
 (custom-set-faces
--- .emacs.d/requires.el
+++ .emacs.d/requires.el
@@ -1,5 +1,12 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;(package-refresh-contents)
+
+(add-to-list
+ 'package-archives
+ '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(when (not package-archive-contents)
+ (package-refresh-contents))
+
 ;(package-install-selected-packages)
-;(package-initialize)
