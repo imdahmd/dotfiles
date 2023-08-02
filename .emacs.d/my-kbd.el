9ce73b044f5aefe981957cfcf2b657fbf4637aa0
Imdad Ahmed <imdad.ahmed@gmail.com>
2023-08-02 13:20:59 +0100

    Add quick switch buffer kbds

--- .emacs.d/my-kbd.el
+++ .emacs.d/my-kbd.el
@@ -1,4 +1,5 @@
-(global-set-key (kbd "s-b") 'quick-list-buffers)
+(global-set-key (kbd "s-B") 'quick-list-buffers)
+(global-set-key (kbd "s-b") 'switch-to-buffer)
 (global-set-key (kbd "s-]") 'goto-next-window)
 (global-set-key (kbd "s-[") 'goto-prev-window)
 (global-set-key (kbd "s-.") 'delete-window)
