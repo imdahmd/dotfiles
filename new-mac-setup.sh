3787e296b1d43233332f0b2e2ee2e3a76e5f1488
Imdad Ahmed <imdad.ahmed@gmail.com>
2026-09-02 22:07:10 +0100

    Modify new mac setup iterm

--- new-mac-setup.sh
+++ new-mac-setup.sh
@@ -34,17 +34,17 @@ fi
 ### 4. Install iTerm2
 brew install --cask iterm2
 
-### Generate new SSH key and add to ssh-agent
-# ssh-keygen -t ed25519 -C "imdad.ahmed@gmail.com"
-# eval "$(ssh-agent -s)"
-# touch ~/.ssh/config
-# cat <<EOF >> ~/.ssh/config
-# Host *
-#   AddKeysToAgent yes
-#   IdentityFile ~/.ssh/id_ed25519
-# EOF
-# ssh-add -K ~/.ssh/id_ed25519
-# pbcopy < ~/.ssh/id_ed25519.pub && echo "SSH public key copied to clipboard"
+# Restore iTerm2 prefs (incl. Option-key-as-Esc+) from the dotfiles repo, if
+# it's already cloned at this point. Otherwise, run this manually after the
+# "Get .dot-files" step below.
+if [ -f "$HOME/.iterm/com.googlecode.iterm2.plist" ]; then
+    cp "$HOME/.iterm/com.googlecode.iterm2.plist" "$HOME/Library/Preferences/"
+    killall cfprefsd 2>/dev/null || true
+else
+    echo "NOTE: ~/.iterm/com.googlecode.iterm2.plist not found yet." >&2
+    echo "      After cloning the dotfiles repo, run:" >&2
+    echo "      cp ~/.iterm/com.googlecode.iterm2.plist ~/Library/Preferences/ && killall cfprefsd" >&2
+fi
 
 ### Get .dot-files from github.com/imdahmd
 # git init .
@@ -52,9 +52,3 @@ brew install --cask iterm2
 # git pull origin master
 # echo "At this point run package-install-selected-packages on emacs to install all packages"
 
-### Install jenv
-# jenv add /usr/local/Cellar/openjdk/17/
-# jenv global 17
-
-### Install babashka
-bash < <(curl -s https://raw.githubusercontent.com/babashka/babashka/master/install)
